c8fad8682a2e8c7929d854ac6132ad2de3c9388c
Daniel <daniel.rueda.salcedo.30@gmail.com>
2026-02-13 20:26:33 -0500

    Se agrega la funcionde multiplicar y archivo main_py

--- funciones.py
+++ funciones.py
@@ -7,7 +7,13 @@ def restar(a,b):
     return a - b
 
 
+def multiplicar(a,b):
+    return a * b
+
+
 print( f'La suma es: {sumar(3,8)}' )
 print( f'La resta es: {restar(11,4)}' )
+print( f'La multiplicación es: {multiplicar(6,7)}' )
+
 
 
